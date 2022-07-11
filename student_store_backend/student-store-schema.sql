9d7ca6d75f067e5ab8a90e7c95fef49c5ce21348
erico07181 <erico@salesforce.com>
2022-07-11 11:02:20 -0700

    step 1 done, created db w SQL

---
+++ student_store_backend/student-store-schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE users (
+  name        TEXT NOT NULL,
+  id          SERIAL PRIMARY KEY,
+  password    TEXT NOT NULL,
+  username    TEXT NOT NULL UNIQUE,
+  email       TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
+  is_admin    BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at  TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+
+CREATE TABLE products (
+  id          SERIAL PRIMARY KEY,
+  name        TEXT NOT NULL UNIQUE,
+  category    TEXT NOT NULL,
+  image       TEXT NOT NULL UNIQUE,
+  description TEXT NOT NULL,
+  price       BIGINT NOT NULL
+);
+
+CREATE TABLE orders (
+  id          SERIAL PRIMARY KEY,
+  customer_id INTEGER NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES users(id) ON DELETE CASCADE,
+  created_at  TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE order_details(
+  order_id    INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+  product_id  INTEGER NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+  quantity    INTEGER NOT NULL DEFAULT 1,
+  discount    INTEGER,
+  PRIMARY KEY (order_id, product_id)
+);
